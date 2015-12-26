b2beb10325fd2665ae061ecc1490dca758e619e9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-12-26 22:16:33 +0100

    Test MeasureOverride

---
+++ samples/XamlTestApplicationPcl/Views/UserControl1.paml.cs
@@ -0,0 +1,28 @@
+﻿using Perspex;
+using Perspex.Controls;
+using Perspex.Markup.Xaml;
+
+namespace XamlTestApplication.Views
+{
+    public class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            PerspexXamlLoader.Load(this);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            var s = base.MeasureOverride(availableSize);
+            sw.Stop();
+            System.Diagnostics.Debug.WriteLine("UserControl1 MeasureOverride " + sw.Elapsed.TotalMilliseconds + "ms");
+            return s;
+        }
+    }
+}
